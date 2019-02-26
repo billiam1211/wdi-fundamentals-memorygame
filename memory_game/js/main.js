f99c18253c7ffd10a1b1a27461cb6f471b79a019
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-02-25 20:20:37 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,25 @@
 console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cards[0]');
-console.log("User flipped " + cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push('cards[1]');
-console.log("User flipped " + cardTwo);
-
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("you found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
+}
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
 if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] = cardsInPlay[1]) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!")
 	} 
 	else {
 		alert("Sorry, try again.")
 	}
+checkForMatch();
 }
-
-
-
+};
+flipCard(0);
+flipCard(2);
