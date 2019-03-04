28b39c4f62382125f53886752595b04eb06639ec
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-03-03 22:42:36 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,29 +25,37 @@ var cards = [
 }
 
 ];
+
 var cardsInPlay = [];
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("you found a match!");
+	alert("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
 	}
 }
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank + ".");
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].cardImage + ".");
-	console.log("User flipped " + cards[cardId].suit + ".");
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
-	} 
-	else {
-		alert("Sorry, try again.")
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
 	}
-checkForMatch();
-}
 };
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+
 
